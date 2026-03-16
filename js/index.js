73496485b238ed4a227560167d68c5c39e918a91
JEROME-1122 <jeromfranklin123@gmail.com>
2026-03-16 18:26:45 +0530

    calculate the left max and right max for each element in the array

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 function calculate() {
   const input = document.getElementById("numb").value;
   console.log(input);
+
+  //convert string into array of numbers
   const numbers = input.split(",").map(Number);
 
-  console.log(numbers);
+    console.log(numbers);
+
+  //calculate the left max and right max for each element in the array
+  let leftMaxNum = [];
+  let rightMaxNum = [];
+  leftMaxNum[0] = numbers[0];
 }
